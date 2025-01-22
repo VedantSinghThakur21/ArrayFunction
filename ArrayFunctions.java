8e87cf66c672bf34822046b70c4187290df45211
unknown <vedant.thakur.btech2023@sitpune.edu.in>
2025-01-22 11:33:34 +0530

    initial commit

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -4,4 +4,24 @@ class ArrayFunctions{
     public void display(int[] array){
         System.out.println("Array is : " + Arrays.toString(array));
     }
+	
+	public void oddEven(int[] array){
+		int[] odd = new int[5];
+		int[] even = new int[5];
+		int ODD_INDEX = 0;
+		int EVEN_INDEX = 0;
+		for(int i = 0; i< array.length; i++){
+			if(array[i]%2 ==0){
+				even[EVEN_INDEX] = array[i];
+				EVEN_INDEX++;
+			}else{
+				if(array[i]%2 ==0){
+				odd[ODD_INDEX] = array[i];
+				ODD_INDEX++;
+			}
+		}
+		
+	}
+	display(odd);
+	display(even);
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         UserInput in = new UserInput(); //Object of UserInput 
         ArrayFunctions functions = new ArrayFunctions(); //Object of ArrayFunctions
-        functions.display(in.arrayInput());
+        //functions.display(in.arrayInput());
+		functions.oddEven(in.arrayInput());
     }
 }

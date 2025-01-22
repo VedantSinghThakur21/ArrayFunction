1a6d5e1ab5b381a752e1a93eca89895d6d30a26f
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-01-22 11:52:06 +0530

    Refined Version

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -1,27 +1,23 @@
-import java.util.Arrays;
+import java.util.*;
 
-class ArrayFunctions{
-    public void display(int[] array){
-        System.out.println("Array is : " + Arrays.toString(array));
+class ArrayFunctions {
+    public void display(ArrayList<Integer> array) {
+        System.out.println("Array is: " + array);
     }
-	
-	public void oddEven(int[] array){
-		int[] odd = new int[5];
-		int[] even = new int[5];
-		int ODD_INDEX = 0;
-		int EVEN_INDEX = 0;
-		for(int i = 0; i< array.length; i++){
-			if(array[i]%2 ==0){
-				even[EVEN_INDEX] = array[i];
-				EVEN_INDEX++;
-			}else{
-				if(array[i]%2 ==0){
-				odd[ODD_INDEX] = array[i];
-				ODD_INDEX++;
-			}
-		}
-		
-	}
-	display(odd);
-	display(even);
+    
+    public void oddEven(int[] array) {
+        ArrayList<Integer> even = new ArrayList<Integer>();
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                even.add(array[i]);
+            } else {
+                odd.add(array[i]); // Corrected this line
+            }
+        }
+        
+        display(odd);
+        display(even);
+    }
 }
